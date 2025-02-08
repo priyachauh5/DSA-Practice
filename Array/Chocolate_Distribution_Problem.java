a9a847ef4433b7fce72e538359c5b3be68008ead
Delta Student <151124074+priyachauh5@users.noreply.github.com>
2025-02-08 23:24:46 +0530

    Array

--- Array/Chocolate_Distribution_Problem.java
+++ Array/Chocolate_Distribution_Problem.java
@@ -13,6 +13,6 @@ public class Chocolate_Distribution_Problem{
     public static void main(String args[]){
         int arr[]={2,3,4,7,9,12,56};
         int m=3;
-        System.out.print("Minimum Diff is: "+chocolateProblem(arr,m));
+        System.out.print("Minimum Diff is: "+chocolateProblem(arr, m));
     }
 }
