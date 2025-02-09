37aa5aa122010b6614395a59d4d8c5caefe8342d
Delta Student <151124074+priyachauh5@users.noreply.github.com>
2025-02-09 23:52:36 +0530

    Array

---
+++ Array/Bit_Manipulation.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public static void main(String args[]) {
+        // Convert the position to 0-based by subtracting 1 from i
+        int position = i - 1;
+        
+        // 1. Get the ith bit
+        int getBit = (num >> position) & 1;
+        
+        // 2. Set the ith bit
+        int setBit = num | (1 << position);
+        
+        // 3. Clear the ith bit
+        int clearBit = num & ~(1 << position);
+        
+        // Output the result in the specified format (space separated)
+        System.out.print(getBit + " " + setBit + " " + clearBit);
+    }
+}
