8edf33b7a8c8436cd1a296e469043eed7d8c71bc
Priya Chauhan <151124074+priyachauh5@users.noreply.github.com>
2025-02-17 03:37:46 +0530

    Array

--- Array/Min_Rotated_Sorted_Array.java
+++ Array/Min_Rotated_Sorted_Array.java
@@ -5,6 +5,7 @@ public class Min_Rotated_Sorted_Array{
         int arr[]={5,7,8,12,15,20,-7,-4,0,2};
         int si=0;
         int ei=arr.length-1;
+        
         while(si<ei){
             int mid=si+(ei-si)/2;
             if(arr[mid]>arr[ei]){
