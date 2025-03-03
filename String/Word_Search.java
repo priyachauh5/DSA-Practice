349f4e473937bbd86416ee01d034153d76aa0fa7
Priya Chauhan <priyachauh5@gmail.com>
2025-03-03 23:44:49 +0530

    String

--- String/Word_Search.java
+++ String/Word_Search.java
@@ -2,6 +2,7 @@ public class Word_Search {
     public static boolean exist(char board[][], String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
+
                 if (backtrack(board, word, i, j, 0)) {
                     return true;
                 }
