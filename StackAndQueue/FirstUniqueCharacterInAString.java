dc819e82a66cbbce166f6d7a81a3dab3e997cb39
Priya Chauhan <priyachauh5@gmail.com>
2025-03-09 21:15:31 +0530

    StackAndQueue

---
+++ StackAndQueue/FirstUniqueCharacterInAString.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.HashMap;
+
+public class FirstUniqueCharacterInAString{
+    public static int unique(String str){
+        HashMap<Character, Integer> s=new HashMap<>();
+
+        for(int i=0; i<str.length(); i++){
+            s.put(str.charAt(i), s.getOrDefault(str.charAt(i),0)+1);
+        }
+
+        for(int i=0; i<str.length(); i++){
+            if(s.get(str.charAt(i))==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        String str="long";
+        System.out.print(unique(str));
+    }
+}
