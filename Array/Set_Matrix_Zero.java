b8737109f75cb85c6c923c6b5800aef93f059587
Priya Chauhan <priyachauh5@gmail.com>
2025-03-12 17:11:58 +0530

    Array

---
+++ Array/Set_Matrix_Zero.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class set_Matrix_Zero{
+    public static void setZero(int matrix[][]){
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j]==0){
+                    
+                }
+            }
+        }
+    }
+    public static void main(String args[]){
+        int matrix[][]={1,2,3,4},{2,3,6,0};
+    }
+}
