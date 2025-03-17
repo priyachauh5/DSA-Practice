93e20daa148ff396f26051e62c6c3b5f501f18c8
Priya Chauhan <priyachauh5@gmail.com>
2025-03-17 23:39:33 +0530

    Array

--- Array/Set_Matrix_Zero.java
+++ Array/Set_Matrix_Zero.java
@@ -4,7 +4,6 @@ public class set_Matrix_Zero{
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
                 if(matrix[i][j]==0){
-
                 }
             }
         }
