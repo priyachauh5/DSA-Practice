ffd8a96fdb3623bea68d550afea351f5798cec68
Priya Chauhan <priyachauh5@gmail.com>
2025-03-19 14:40:50 +0530

    Array

---
+++ Array/AddDigit.java
@@ -0,0 +1,18 @@
+public class AddDigit{
+    public static int AddDigit(int num){
+        while(num>=10){
+             int sum=0;
+            while(num>0){
+                int rem=num%10;
+                sum+=rem;
+                num=num/10;
+            }
+            num=sum;
+        }
+        return num;
+    }
+    public static void main(String args[]){
+        int  num=38;
+        System.out.print(AddDigit(num));
+    }
+}
